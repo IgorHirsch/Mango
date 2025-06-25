90bdd10bdd0fb09688ccc5cdef18d656f9cf14f2
IgorHirsch <g1rsh.egor@gmail.com>
2025-06-25 12:45:09 +0200

    Auth API

---
+++ Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.AuthAPI.Models;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDTO registrationRequestDto);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDto);
+        Task<bool> AssignRole(string email, string roleName);
+    }
+}
